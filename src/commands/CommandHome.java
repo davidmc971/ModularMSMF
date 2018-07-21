86c8fadb8340767fa6f677589ec5a5fbbcecf5d2
Lightkeks <alardeux@live.de>
2018-07-21 14:24:27 +0200

    home changed

--- src/commands/CommandHome.java
+++ src/commands/CommandHome.java
@@ -36,19 +36,39 @@ public class CommandHome extends AbstractCommand {
 		if(args.length > 0) {
 			switch (args[0].toLowerCase()) {
 			case "list":
-				
+				if(sender.hasPermission(PermissionManager.getPermission("home_list"))) {
+					//src for listing all homes set by it's own. admin can list player's home's for itself
+				} else {
+					sender.sendMessage(noPermPrefix+language.getString("general.nopermission"));
+				}
 				break;
 			case "set":
-	
+				if(sender.hasPermission(PermissionManager.getPermission("home_set"))) {
+					//src for setting a home's player. admin can set on it's own position a new home for a player too.
+				} else {
+					sender.sendMessage(noPermPrefix+language.getString("general.nopermission"));
+				}
 				break;
 			case "remove":
-	
+				if(sender.hasPermission(PermissionManager.getPermission("home_remove"))) {
+					//src for removing home(s) which has been set, otherwise admin can remove selected or all homes from a player
+				} else {
+					sender.sendMessage(noPermPrefix+language.getString("general.nopermission"));
+				}
 				break;
 			case "rtp":
-	
+				if(sender.hasPermission(PermissionManager.getPermission("home_rtp"))) {
+					//src for requesting an teleport for another home's location from a player who's online. admin can w/o requesting.
+				} else {
+					sender.sendMessage(noPermPrefix+language.getString("general.nopermission"));
+				}
 				break;
 			default:
-	
+				//src for teleporting to its home or any home which has been labeled with a custom name, which was defined under "set"
+				/** } else {
+				 * sender.sendMessage(errorPrefix+"language.getString("commands.home.notfoundhome"));
+				 * }
+				 */
 				break;
 			}
 			
