e5273a390644149772e6bc6215aea932d12f42a3
Lightkeks <alardeux@live.de>
2018-07-21 13:49:55 +0200

    home change

--- src/commands/CommandHome.java
+++ src/commands/CommandHome.java
@@ -33,62 +33,23 @@ public class CommandHome extends AbstractCommand {
 		// TODO: Implementieren
 		// Playerdata plrdat =
 
-		switch (label.toLowerCase()) {
-		case "home":
-			if(sender.hasPermission(PermissionManager.getPermission("home"))) {
-				//src for home spawning
-				if(sender.hasPermission(PermissionManager.getPermission("home_list"))) {
-					if(args.length == 1 || args.equals("list")){
-						/*
-						 * LOGISCHE OPERATOREN
-						 * ODER									||
-						 * UND									&&
-						 * KLEINER, KLEINER GLEICH				<	<=
-						 * GROESSER								>	>=
-						 * GLEICH								==
-						 * NICHT GLEICH							!=
-						 * NEGIERT (boolean)					!
-						 * 
-						 * GRUNDLAGEN ARRAYS:
-						 * 
-						 * args ist oben im Methodenkopf als
-						 * String[] args
-						 * definiert
-						 * 
-						 * args bzw. args.irgendwas benutzt du um auf die Eigenschaften des Arrays zuzugreifen (args.length bspw.)
-						 * 
-						 * args[index] bzw. args[index].irgendwas benutzt du um auf den [index]ten Eintrag im Array zuzugreifen
-						 * 
-						 * BEISPIEL:
-						 * 
-						 * bei /home set basis2
-						 * 
-						 * args.length == 2
-						 * args[0] ist "set"
-						 * args[1] ist "basis2"
-						 * args[2] existiert nicht
-						 */
-						//listing all homes which the player has set
-					} else {
-						sender.sendMessage(errorPrefix+"commands.home.notset");
-					}
-				} else {
-					sender.sendMessage(noPermPrefix+"general.nopermission");
-				}
-			} else {
-				sender.sendMessage(noPermPrefix+"general.nopermission");
-			}
+		if(args.length == 0) {
+			switch (label.toLowerCase()) {
+			case "list":
+				
 				break;
-		case "sethome":
-
-			break;
-		case "delhome":
-
-			break;
+			case "sethome":
+	
+				break;
+			case "delhome":
+	
+				break;
+			}
+			
 		}
 		return true;
 	}
-
+	
 	@Override
 	public String getCommandLabel() {
 		return "home";
