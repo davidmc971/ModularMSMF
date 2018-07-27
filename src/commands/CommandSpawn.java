110cde45d800c710beb400ff8a0cab30fe239846
Lightkeks <alardeux@live.de>
2018-07-27 11:48:51 +0200

    added language

--- src/commands/CommandSpawn.java
+++ src/commands/CommandSpawn.java
@@ -26,11 +26,11 @@ public class CommandSpawn extends AbstractCommand {
 	public CommandSpawn(ModularMSMF plugin) {
 		super(plugin);
 	}
-	
+
 	/**
 	 * @TODO Complete rewrite
 	 */
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
@@ -39,44 +39,42 @@ public class CommandSpawn extends AbstractCommand {
 		String errorPrefix = ChatUtils.getFormattedPrefix(ChatUtils.ChatFormat.ERROR);
 		String noPermPrefix = ChatUtils.getFormattedPrefix(ChatUtils.ChatFormat.NOPERM);
 		String successfulPrefix = ChatUtils.getFormattedPrefix(ChatUtils.ChatFormat.SUCCESS);
-		
+
 		if(sender instanceof ConsoleCommandSender) {
 			sender.sendMessage(noPermPrefix+language.getString("general.noconsole"));
 		} else {
-		Player p = (Player)sender;
-		
-		if(!p.hasPermission("spawn.spawn")){
-			p.sendMessage("Du hast keine Rechte!");
-		}
-		
-		File file = new File("plugins/ModularMSMF/spawnconfig.yml");
-		if(!file.exists()){
-			p.sendMessage("Es wurde kein Spawn gesetzt");
-		}
-		
-		YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
-		double x = cfg.getDouble("X");
-		double y = cfg.getDouble("Y");
-		double z = cfg.getDouble("Z");
-		double yaw = cfg.getDouble("Yaw");
-		double pitch = cfg.getDouble("Pitch");
-		String worldname = cfg.getString("Worldname");
-		
-		Location loc = p.getLocation();
-		
-		loc.setX(x);
-		loc.setY(y);
-		loc.setZ(z);
-		loc.setYaw((float)yaw);
-		loc.setPitch((float)pitch);
-		
-		World welt = Bukkit.getWorld(worldname);
-		loc.setWorld(welt);
-		
-		p.teleport(loc);
-		p.sendMessage(successfulPrefix+language.getString("spawn.spawned")+"Du wurdest gespawnt!");
-		
-		return true;
+			Player p = (Player)sender;
+
+			if(!p.hasPermission("spawn.spawn")){
+				p.sendMessage("Du hast keine Rechte!");
+			}
+
+			File file = new File("plugins/ModularMSMF/spawnconfig.yml");
+			if(!file.exists()){
+				p.sendMessage("Es wurde kein Spawn gesetzt");
+			}
+
+			YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+			double x = cfg.getDouble("X");
+			double y = cfg.getDouble("Y");
+			double z = cfg.getDouble("Z");
+			double yaw = cfg.getDouble("Yaw");
+			double pitch = cfg.getDouble("Pitch");
+			String worldname = cfg.getString("Worldname");
+
+			Location loc = p.getLocation();
+
+			loc.setX(x);
+			loc.setY(y);
+			loc.setZ(z);
+			loc.setYaw((float)yaw);
+			loc.setPitch((float)pitch);
+
+			World welt = Bukkit.getWorld(worldname);
+			loc.setWorld(welt);
+
+			p.teleport(loc);
+			p.sendMessage(successfulPrefix+language.getString("spawn.spawned")+"Du wurdest gespawnt!"); //gibt NULL aus, warum?
 		}
 		return true;
 	}
