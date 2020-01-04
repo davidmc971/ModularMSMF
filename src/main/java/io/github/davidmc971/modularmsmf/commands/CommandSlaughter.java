d3adfb191d5ad03a7da2e3b7b49ed8ba2a61ef95
davidmc971 <david.pfeiffer971@gmail.com>
2020-01-04 07:10:08 +0100

    work on slaughter

--- src/main/java/io/github/davidmc971/modularmsmf/commands/CommandSlaughter.java
+++ src/main/java/io/github/davidmc971/modularmsmf/commands/CommandSlaughter.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
@@ -35,12 +36,12 @@ public class CommandSlaughter extends AbstractCommand {
 		 */
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		
 		FileConfiguration language = Utils.configureCommandLanguage(sender, plugin);
+
 		if(args.length == 0){
 			if(sender.hasPermission(PermissionManager.getPermission("slaughter"))){
 				if(sender instanceof ConsoleCommandSender){
-					sender.sendMessage(errorPrefix+"ERROR"); //for testing purposes only
+					Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatUtils.ChatFormat.ERROR, "general.noconsole");
 				}
 				else if(sender instanceof Player) {
 					Location playerloc = ((Player) sender).getLocation();
@@ -51,7 +52,21 @@ public class CommandSlaughter extends AbstractCommand {
 					sender.sendMessage(successfulPrefix+language.getString("commands.slaughter.success"));
 				}
 			}
-		} else if(args.length >= 1){
+		} else if(args.length == 1 && args[0].equalsIgnoreCase("passive")){
+			if(sender.hasPermission(PermissionManager.getPermission("slaughter"))){
+				if(sender instanceof ConsoleCommandSender){
+					Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatUtils.ChatFormat.ERROR, "general.noconsole");
+				}
+				else if(sender instanceof Player) {
+					Location playerloc = ((Player) sender).getLocation();
+					for (Entity e : ((Player) sender).getWorld().getNearbyEntities(playerloc, 500, 500, 500)) {
+						if (!(e instanceof Player) && (e instanceof Animals))
+							e.remove();
+					}
+					sender.sendMessage(successfulPrefix+language.getString("commands.slaughter.success"));
+				}
+			}
+		} else {
 			sender.sendMessage(errorPrefix+language.getString("general.toomanyarguments"));
 		}
 		return true;
