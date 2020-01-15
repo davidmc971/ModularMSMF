ab5cfd9911fe4ac24a4f5f02401e553090644d82
Lightkeks <alardeux@live.de>
2020-01-15 09:47:50 +0100

    unfinished code

--- src/main/java/io/github/davidmc971/modularmsmf/commands/CommandClearChat.java
+++ src/main/java/io/github/davidmc971/modularmsmf/commands/CommandClearChat.java
@@ -41,10 +41,10 @@ public class CommandClearChat extends AbstractCommand{
                 if(sender.hasPermission(PermissionManager.getPermission("clear_other"))){
                     int count = 0;
                     target = Utils.getPlayerUUIDByName(args[0]);
-                    if(target == null){
-                        Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "general.playernotfound");
-                        return true;
-                    }
+                    //if(target == null){
+                    //    Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "general.playernotfound");
+                    //    return true;
+                    //}
                     for (Player p : Bukkit.getOnlinePlayers()) {
                         if (p.getUniqueId().toString().equalsIgnoreCase(target.toString())) {
                             if(sender == p){
@@ -55,7 +55,7 @@ public class CommandClearChat extends AbstractCommand{
                                 if(count==99){
                                     Utils.sendMessageWithConfiguredLanguage(plugin, p, ChatFormat.SUCCESS, "commands.clear.gotcleared");
                                 }
-                            } else {
+                            } else
                                 while (count!=99){
                                     count++;
                                     p.sendMessage("");
@@ -64,20 +64,19 @@ public class CommandClearChat extends AbstractCommand{
                                     Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS, "commands.clear.target", "_target", p.getDisplayName());
                                     Utils.sendMessageWithConfiguredLanguage(plugin, p, ChatFormat.SUCCESS, "commands.clear.gotcleared");
                                 }
+                            } else {
+                                Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "general.playernotonline");
                             }
-                        } else {
-                            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "general.playernotonline");
+                            return true;
                         }
-                        return true;
+                    } else {
+                        Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.NOPERM, "general.nopermission");
                     }
-                } else {
-                    Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.NOPERM, "general.nopermission");
-                }
+                    break;
+                default:
+                    Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "general.toomanyarguments");
                 break;
-            default:
-                Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "general.toomanyarguments");
-            break;
-        }
+            }
         return true;
     }
 
