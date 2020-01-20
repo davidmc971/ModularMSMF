70d833969d81bde2a61bf8b663adde26447f6f07
Lightkeks <alardeux@live.de>
2020-01-20 12:55:50 +0100

    Update Events.java

--- src/main/java/io/github/davidmc971/modularmsmf/listeners/Events.java
+++ src/main/java/io/github/davidmc971/modularmsmf/listeners/Events.java
@@ -155,32 +155,32 @@ public class Events implements Listener {
 		hardCodedBlackList.add("hacker");
 		for(int i = 0; i < hardCodedBlackList.size(); i++){
 			if(!hardCodedBlackList.contains(msg)) {
-			//plugin.getLogger().info("Async Chat Event");
-			FileConfiguration playercfg = plugin.getDataManager().getPlayerCfg(event.getPlayer().getUniqueId());
-			if (playercfg.isBoolean("muted") && playercfg.getBoolean("muted") && !event.getMessage().startsWith("/")) {
-				event.setCancelled(true);
-				Utils.sendMessageWithConfiguredLanguage(plugin, event.getPlayer(), ChatFormat.NOPERM, "event.muted");
-			}
+				//plugin.getLogger().info("Async Chat Event");
+				FileConfiguration playercfg = plugin.getDataManager().getPlayerCfg(event.getPlayer().getUniqueId());
+				if (playercfg.isBoolean("muted") && playercfg.getBoolean("muted") && !event.getMessage().startsWith("/")) {
+					event.setCancelled(true);
+					Utils.sendMessageWithConfiguredLanguage(plugin, event.getPlayer(), ChatFormat.NOPERM, "event.muted");
+				}
 
-			FileConfiguration settings = plugin.getDataManager().settingsyaml; //TODO: change to getter
-			ChatColor cl_prefix = toColor(settings, "chat.colors.prefix");
-			ChatColor cl_name = toColor(settings, "chat.colors.displayname");
-			ChatColor cl_msg = toColor(settings, "chat.colors.message");
+				FileConfiguration settings = plugin.getDataManager().settingsyaml; //TODO: change to getter
+				ChatColor cl_prefix = toColor(settings, "chat.colors.prefix");
+				ChatColor cl_name = toColor(settings, "chat.colors.displayname");
+				ChatColor cl_msg = toColor(settings, "chat.colors.message");
 
-			//l.info("cl_prefix: " + cl_prefix);
-			//l.info("cl_name: " + cl_name);
-			//l.info("cl_msg: " + cl_msg);
+				//l.info("cl_prefix: " + cl_prefix);
+				//l.info("cl_name: " + cl_name);
+				//l.info("cl_msg: " + cl_msg);
 
-			String format = settings.getString("chat.format");
-			//l.info("format #1: " + format);
-			format = format.replaceAll("_name", "%1\\$s")
-				.replaceAll("_message", "%2\\$s")
-				.replaceAll("_clpre", cl_prefix.toString())
-				.replaceAll("_clname", cl_name.toString())
-				.replaceAll("_clmessage", cl_msg.toString());
-			//l.info("format #2: " + format);
+				String format = settings.getString("chat.format");
+				//l.info("format #1: " + format);
+				format = format.replaceAll("_name", "%1\\$s")
+					.replaceAll("_message", "%2\\$s")
+					.replaceAll("_clpre", cl_prefix.toString())
+					.replaceAll("_clname", cl_name.toString())
+					.replaceAll("_clmessage", cl_msg.toString());
+				//l.info("format #2: " + format);
 
-			event.setFormat(format);
+				event.setFormat(format);
 			} else {
 
 				for(String block : hardCodedBlackList){
