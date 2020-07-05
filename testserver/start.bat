1ba6c8d7cddf14eef48954dba4914d1cee178d70
davidmc971 <david.pfeiffer971@gmail.com>
2020-07-05 11:34:10 +0000

    server script version up

--- testserver/start.bat
+++ testserver/start.bat
@@ -1,5 +1,5 @@
 @echo off
 :1
-java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx4G -Xms4G -jar spigot-1.15.1.jar
+java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx4G -Xms4G -jar spigot-1.15.2.jar
 goto 1
 pause
--- testserver/start.sh
+++ testserver/start.sh
@@ -1,4 +1,4 @@
-until java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx4G -Xms4G -jar spigot-1.15.1.jar; do
+until java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx4G -Xms4G -jar spigot-1.15.2.jar; do
     echo "Server 'myserver' crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
