b84fe5fe39fdfa7e26299ace8d04fb28a0966ccf
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2021-03-21 12:24:43 +0100

    different file copy in commandblocker

--- ModularMSMF-CommandBlocker/src/main/java/io/github/davidmc971/modularmsmf/configuration/BlockedCommands.java
+++ ModularMSMF-CommandBlocker/src/main/java/io/github/davidmc971/modularmsmf/configuration/BlockedCommands.java
@@ -3,6 +3,7 @@ package io.github.davidmc971.modularmsmf.configuration;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import org.bukkit.configuration.file.FileConfiguration;
@@ -40,7 +41,12 @@ public class BlockedCommands {
                 file.getParentFile().mkdirs();
                 file.createNewFile();
                 FileOutputStream fos = new FileOutputStream(file);
-                fos.write(ModularMSMFCommandBlocker.Instance().getResource("blockedcommands.yml").readAllBytes());
+                InputStreamReader reee = new InputStreamReader(ModularMSMFCommandBlocker.Instance().getResource("blockedcommands.yml"));
+                while (reee.ready()) {
+                    fos.write(reee.read());
+                }
+                // fos.write(ModularMSMFCommandBlocker.Instance().getResource("blockedcommands.yml").readAllBytes());
+                reee.close();
                 fos.close();
             } catch (IOException e) {
                 e.printStackTrace();
