0cc502d1a7d57ff4e5204cdd8b0c6a058430ab31
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2021-03-21 15:05:12 +0100

    Version bump and some javadoc
    Version 0.3.0 marks the introduction of the API

--- ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/api/ILanguage.java
+++ ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/api/ILanguage.java
@@ -1,28 +1,33 @@
 package io.github.davidmc971.modularmsmf.api;
 
 /**
- * An Interface to represent the key requirements for a language to be used in ModularMSMF.
+ * An Interface to represent the key requirements for a language to be used in
+ * ModularMSMF.
+ * 
+ * @author davidmc971 (David Alexander Pfeiffer)
+ * @since 0.3.0
  */
 public interface ILanguage {
     /**
      * Returns the Language ID. For example "en_US".
+     * 
      * @return Language ID
      */
     public String ID();
 
     /**
      * Returns the written name of the language. For example "English".
+     * 
      * @return Language name
      */
     public String Name();
 
     /**
-     * Used to loalize Strings throughout ModularMSMF.
-     * For Example:
-     * "general.toomanyarguments" in language "en_US" returns
-     * "Too many arguments."
-     * @param localizationKey The key for the language,
-     *  usually the YAML path or JSON path depending on the language implementation.
+     * Used to loalize Strings throughout ModularMSMF. For Example:
+     * "general.toomanyarguments" in language "en_US" returns "Too many arguments."
+     * 
+     * @param localizationKey The key for the language, usually the YAML path or
+     *                        JSON path depending on the language implementation.
      * @return The localized String.
      */
     public String Localize(String localizationKey);
