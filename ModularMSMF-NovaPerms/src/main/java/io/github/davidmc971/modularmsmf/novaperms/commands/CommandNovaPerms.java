fa6a4099badd4200edfb62db9c250cdf3151b03a
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2021-03-26 21:03:05 +0100

    subcommand methods

--- ModularMSMF-NovaPerms/src/main/java/io/github/davidmc971/modularmsmf/novaperms/commands/CommandNovaPerms.java
+++ ModularMSMF-NovaPerms/src/main/java/io/github/davidmc971/modularmsmf/novaperms/commands/CommandNovaPerms.java
@@ -21,52 +21,59 @@ public class CommandNovaPerms implements IModularMSMFCommand {
             sender.sendMessage(Component.text("No permission."));
             return true;
         }
-
-        for (String string : args) {
-            string = string.toLowerCase();
-        }
         // switch for subcommands
-        switch (args[0]) {
+        switch (args[0].toLowerCase()) {
         case "groups":
         case "g":
             // list available groups
+            novaperms_groups(sender, command, label, args);
             break;
         case "inspect":
         case "info":
         case "i":
             // inspect group or player to get detailed info
+            novaperms_inspect(sender, command, label, args);
             break;
         case "assign":
         case "add":
         case "a":
             // assign a player to a group
+            novaperms_assign(sender, command, label, args);
             break;
         case "permit":
         case "allow":
         case "p":
             // permit a group or player for a specific permission
+            novaperms_permit(sender, command, label, args);
             break;
         case "create":
         case "new":
         case "c":
             // create a new group
+            novaperms_create(sender, command, label, args);
             break;
         case "prefix":
             // set a group's prefix
+            novaperms_prefix(sender, command, label, args);
             break;
         case "forbid":
         case "remove":
         case "rm":
         case "r":
             // remove a permission from a group or user
+            novaperms_forbid(sender, command, label, args);
             break;
         case "delete":
         case "del":
         case "d":
             // remove player from all groups or delete a group
+            novaperms_delete(sender, command, label, args);
             break;
+        default:
+            // unknown subcommand
+            return false;
         }
-        return false;
+        return true;
     }
 
     @Override
@@ -74,4 +81,35 @@ public class CommandNovaPerms implements IModularMSMFCommand {
         return new String[] { "novaperms", "nperms", "novap", "nope", "np" };
     }
 
+    private void novaperms_groups(CommandSender sender, Command command, String label, String[] args) {
+        // TODO: implementation
+    }
+
+    private void novaperms_inspect(CommandSender sender, Command command, String label, String[] args) {
+        // TODO: implementation
+    }
+
+    private void novaperms_assign(CommandSender sender, Command command, String label, String[] args) {
+        // TODO: implementation
+    }
+
+    private void novaperms_permit(CommandSender sender, Command command, String label, String[] args) {
+        // TODO: implementation
+    }
+
+    private void novaperms_create(CommandSender sender, Command command, String label, String[] args) {
+        // TODO: implementation
+    }
+
+    private void novaperms_prefix(CommandSender sender, Command command, String label, String[] args) {
+        // TODO: implementation
+    }
+
+    private void novaperms_forbid(CommandSender sender, Command command, String label, String[] args) {
+        // TODO: implementation
+    }
+
+    private void novaperms_delete(CommandSender sender, Command command, String label, String[] args) {
+        // TODO: implementation
+    }
 }
