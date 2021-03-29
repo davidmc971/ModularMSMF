e70965ddfb10f3c5cf3adfcc8abd1758720d3bb5
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2021-03-29 18:43:58 +0200

    apper server version bump for testserver

--- testserver/start.bat
+++ testserver/start.bat
@@ -1,5 +1,5 @@
 @echo off
 :1
-java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx4G -Xms4G -jar paper-1.16.5-550.jar nogui
+java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx4G -Xms4G -jar paper-1.16.5-573.jar nogui
 goto 1
 pause
--- testserver/start.sh
+++ testserver/start.sh
@@ -1,4 +1,4 @@
-until java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx4G -Xms4G -jar paper-1.16.5-550.jar; do
+until java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx4G -Xms4G -jar paper-1.16.5-573.jar; do
     echo "Server 'myserver' crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
