686882634020d9565c3e93ab7c30d97cb23ab0e9
Lightkeks <alexandrelardeux1996@gmail.com>
2021-03-30 23:26:59 +0200

    Revert "cleaned up code"
    This reverts commit 78a25fc1c6624a538b6c5815672759dd0ad22f60.

--- ModularMSMF-Basics/src/main/java/io/github/davidmc971/modularmsmf/basics/commands/CommandToggle.java
+++ ModularMSMF-Basics/src/main/java/io/github/davidmc971/modularmsmf/basics/commands/CommandToggle.java
@@ -1,8 +1,11 @@
 package io.github.davidmc971.modularmsmf.basics.commands;
 
+import java.util.UUID;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.jetbrains.annotations.NotNull;
 
 import io.github.davidmc971.modularmsmf.api.IModularMSMFCommand;
 import io.github.davidmc971.modularmsmf.core.ModularMSMFCore;
@@ -36,7 +39,7 @@ public class CommandToggle implements IModularMSMFCommand {
                 case "back":
                     if (cfg.isSet("toggle.commands")) {
                         if (cfg.isSet("toggle.commands.back")) {
-                            if (cfg.get("toggle.commands.back").toString().equals(false)) {
+                            if (cfg.get("toggle.commands.back").toString().equals("false")) {
                                 cfg.set("toggle.commands.back", true);
                                 Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS,
                                         "basics.toggle.setTrue", "_args", args[0]);
@@ -48,7 +51,7 @@ public class CommandToggle implements IModularMSMFCommand {
                             break;
                         } else {
                             Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "general.missingconfig.substring");
-                            cfg.set("toggle.commands.back", false);
+                            cfg.set("toggle.commands.back", "false");
                         }
                     } else {
                         Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR,
