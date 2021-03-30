d5953e05e7938e48a45f5bc5548b938d004e20b6
Lightkeks <alexandrelardeux1996@gmail.com>
2021-03-30 21:00:09 +0200

    toggle for back works

--- ModularMSMF-Basics/src/main/java/io/github/davidmc971/modularmsmf/basics/commands/CommandToggle.java
+++ ModularMSMF-Basics/src/main/java/io/github/davidmc971/modularmsmf/basics/commands/CommandToggle.java
@@ -1,5 +1,7 @@
 package io.github.davidmc971.modularmsmf.basics.commands;
 
+import java.util.UUID;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -20,22 +22,45 @@ public class CommandToggle implements IModularMSMFCommand {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
-            @NotNull String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
         FileConfiguration cfg = plugin.getDataManager().settingsyaml;
         if (PermissionManager.checkPermission(sender, "toggle_use")) {
-            switch (args[0].toLowerCase()) {
-            case "help":
-            break;
-            case "back":
-                if(cfg.get("toggle.commands.back").toString().equals("false")){
-                    cfg.set("toggle.commands.back", true);
-                    Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS, "basics.toggle.setTrue", "_args", args[0]);
-                } else {
-                    cfg.set("toggle.commands.back", false);
-                    Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS, "basics.toggle.setFalse", "_args", args[0]);
+            if (args.length == 0) {
+                Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "general.missingarguments");
+            } else if (args.length == 1) {
+                switch (args[0].toLowerCase()) {
+                default:
+                    Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR,
+                            "general.invalidargument");
+                    break;
+                case "help":
+                    break;
+                case "back":
+                    if (cfg.isSet("toggle.commands")) {
+                        if (cfg.isSet("toggle.commands.back")) {
+                            if (cfg.get("toggle.commands.back").toString().equals("false")) {
+                                cfg.set("toggle.commands.back", true);
+                                Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS,
+                                        "basics.toggle.setTrue", "_args", args[0]);
+                            } else {
+                                cfg.set("toggle.commands.back", false);
+                                Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS,
+                                        "basics.toggle.setFalse", "_args", args[0]);
+                            }
+                            break;
+                        } else {
+                            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "general.missingconfig.substring");
+                            cfg.set("toggle.commands.back", "false");
+                        }
+                    } else {
+                        Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR,
+                                "general.missingconfig");
+                        cfg.createSection("toggle.commands");
+                    }
                 }
-                break;
+            } else {
+                Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "general.toomanyarguments");
             }
         } else {
             Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.NOPERM, "general.nopermission");
