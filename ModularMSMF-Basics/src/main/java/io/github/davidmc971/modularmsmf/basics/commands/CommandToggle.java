8d9f7a2950478b3a0f6f208d1ed363542d2b3046
Lightkeks <alexandrelardeux1996@gmail.com>
2021-04-02 18:52:08 +0200

    yes

--- ModularMSMF-Basics/src/main/java/io/github/davidmc971/modularmsmf/basics/commands/CommandToggle.java
+++ ModularMSMF-Basics/src/main/java/io/github/davidmc971/modularmsmf/basics/commands/CommandToggle.java
@@ -1,8 +1,11 @@
 package io.github.davidmc971.modularmsmf.basics.commands;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.MemoryConfiguration;
 
 import io.github.davidmc971.modularmsmf.api.IModularMSMFCommand;
 import io.github.davidmc971.modularmsmf.core.ModularMSMFCore;
@@ -18,31 +21,41 @@ public class CommandToggle implements IModularMSMFCommand {
         plugin = ModularMSMFCore.Instance();
     }
 
+    public Set<String> togglecmds = new LinkedHashSet<String>();
+
+    public static void togglecmds(String command) {
+
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // FIXME: commands won't load properly
+        //// FIXME: commands won't load properly
         // @bug --^---
         if (!PermissionManager.checkPermission(sender, "toggle_use")) {
             Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.NOPERM, "general.nopermission");
             return true;
         }
-        if(args.length < 1){
+        if (args.length < 1) {
             return helpSub(sender, command, label, args);
+        }
+        if (args.length > 3) {
+            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "general.toomanyarguments");
+            return true;
         }
         switch (args[0].toLowerCase()) {
         case "help":
-            return helpSub(sender, command, label, args);
+            return helpSub(sender, command, label, args); //TODO: have to add
         case "all":
-            return allSub(sender, command, label, args);
+            return allSub(sender, command, label, args); //TODO: have to add
         case "teleport":
-            return teleportSub(sender, command, label, args);
+            return teleportSub(sender, command, label, args); //TODO: have to add
         case "setspawn":
-            return setspawnSub(sender, command, label, args);
+            return setspawnSub(sender, command, label, args); //TODO: have to add
         case "back":
-            return backSub(sender, command, label, args);
+            return backSub(sender, command, label, args); //TODO: have to add
         case "test":
-            return testSub(sender, command, label, args);
-        default:
+            return testSub(sender, command, label, args); //TODO: have to add
+        default: //if args[0] does not equal any case labels
             Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "general.invalidarguments");
             break;
         }
@@ -55,90 +68,94 @@ public class CommandToggle implements IModularMSMFCommand {
     }
 
     private boolean backSub(CommandSender sender, Command command, String commandLabel, String[] args) {
-        FileConfiguration cfg = plugin.getDataManager().settingsyaml;
+        // checking if teleport has been added to list
+        if (!togglecmds.contains(args[0])) {
+            //if its not set in list
+            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "basics.toggle.setTrue", "_args", args[0]);
+            togglecmds.add(args[0]);
+        } else {
+            //else it was already in list
+            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "basics.toggle.setFalse", "_args", args[0]);
+            togglecmds.remove(args[0]);
+        }
+        //only for debug stuff
+        System.out.println(togglecmds);
 
-        // if (cfg.isSet("toggle.commands")) { // FIXME: string has to be loaded when
-        // module Basics is loaded
-        // if (cfg.isSet("toggle.commands.back")) {
-        // if (cfg.get("toggle.commands.back").toString().equals("false")) {
-        // cfg.set("toggle.commands.back", "true");
-        Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS, "basics.toggle.setTrue", "_args",
-                args[0]);
-        // } else {
-        // cfg.set("toggle.commands.back", "false");
-        Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS, "basics.toggle.setFalse", "_args",
-                args[0]);
-        // }
-        // } else {
-        Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "basics.missingconfig.substring");
-        // cfg.set("toggle.commands.back", "true");
-        // }
-        // } else {
-        Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "basics.missingconfig.mainstring");
-        // cfg.createSection("toggle.commands");
-        // }
         return true;
     }
 
     private boolean setspawnSub(CommandSender sender, Command command, String commandLabel, String[] args) {
-        FileConfiguration cfg = plugin.getDataManager().settingsyaml;
-        if (cfg.isSet("toggle.commands")) { // FIXME: string has to be loaded when module Basics is loaded
-            if (cfg.isSet("toggle.commands.setspawn")) {
-                if (cfg.get("toggle.commands.setspawn").toString().equals("false")) {
-                    cfg.set("toggle.commands.setspawn", "true");
-                    Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS, "basics.toggle.setTrue",
-                            "_args", args[0]);
-                } else {
-                    cfg.set("toggle.commands.setspawn", "false");
-                    Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS,
-                            "basics.toggle.setFalse", "_args", args[0]);
-                }
-            } else {
-                Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG,
-                        "basics.missingconfig.substring");
-                cfg.set("toggle.commands.setspawn", "true");
-            }
+        // checking if teleport has been added to list
+        if (!togglecmds.contains("setspawn")) {
+            //if its not set in list
+            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "basics.toggle.setTrue", "_args", args[0]);
+            togglecmds.add("setspawn");
         } else {
-            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG,
-                    "basics.missingconfig.mainstring");
-            cfg.createSection("toggle.commands");
+            //else it was already in list
+            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "basics.toggle.setFalse", "_args", args[0]);
+            togglecmds.remove("setspawn");
         }
+        //only for debug stuff
+        System.out.println(togglecmds);
+
         return true;
     }
 
     private boolean teleportSub(CommandSender sender, Command command, String commandLabel, String[] args) {
-        FileConfiguration cfg = plugin.getDataManager().settingsyaml;
-        if (cfg.isSet("toggle.commands")) { // FIXME: string has to be loaded when module Basics is loaded
-            if (cfg.isSet("toggle.commands.teleport")) {
-                if (cfg.get("toggle.commands.teleport").toString().equals("false")) {
-                    cfg.set("toggle.commands.teleport", "true");
-                    Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS, "basics.toggle.setTrue",
-                            "_args", args[0]);
-                } else {
-                    cfg.set("toggle.commands.teleport", "false");
-                    Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS,
-                            "basics.toggle.setFalse", "_args", args[0]);
-                }
-            } else {
-                Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG,
-                        "basics.missingconfig.substring");
-                cfg.set("toggle.commands.teleport", "true");
-            }
+        // checking if teleport has been added to list
+        if (!togglecmds.contains("teleport")) {
+            //if its not set in list
+            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "basics.toggle.setTrue", "_args", args[0]);
+            togglecmds.add("teleport");
         } else {
-            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG,
-                    "basics.missingconfig.mainstring");
-            cfg.createSection("toggle.commands");
+            //else it was already in list
+            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "basics.toggle.setFalse", "_args", args[0]);
+            togglecmds.remove("teleport");
         }
+        //only for debug stuff
+        System.out.println(togglecmds);
+
         return true;
     }
 
     private boolean allSub(CommandSender sender, Command command, String commandLabel, String[] args) {
-        return false;
+        // checking if teleport has been added to list
+        if (!togglecmds.contains("all-module-basic")) {
+            //if its not set in list
+            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "basics.toggle.setTrue", "_args", args[0]);
+            togglecmds.add("all-module-basic");
+        } else {
+            //else it was already in list
+            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG, "basics.toggle.setFalse", "_args", args[0]);
+            togglecmds.remove("all-module-basic");
+        }
+        //only for debug stuff
+        System.out.println(togglecmds);
+
+        return true;
     }
 
     private boolean helpSub(CommandSender sender, Command command, String commandLabel, String[] args) {
-        sender.sendMessage("help here");
+        Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.INFO, "basics.toggle.help");
+        /*
+         * MemoryConfiguration cfg = plugin.getDataManager().settingsyaml;
+         *
+         * if(!cfg.isSet("toggle.commands")){
+         * Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG,
+         * "basics.missingconfig.mainstring"); cfg.createSection("toggle.commands");
+         * return true; } if(!cfg.isSet("toggle.commands.help")){
+         * Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG,
+         * "basics.missingconfig.substring"); cfg.set("toggle.commands.help", "true");
+         * return true; } if
+         * (!cfg.get("toggle.commands.help").toString().equals("false")) {
+         * Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.DEBUG,
+         * "basics.toggle.setFalse", "_args", args[0]); cfg.set("toggle.commands.help",
+         * "false"); } else { Utils.sendMessageWithConfiguredLanguage(plugin, sender,
+         * ChatFormat.DEBUG, "basics.toggle.setTrue", "_args", args[0]);
+         * cfg.set("toggle.commands.help", "true"); return true; }
+         */
         return true;
+
     }
 
     @Override
