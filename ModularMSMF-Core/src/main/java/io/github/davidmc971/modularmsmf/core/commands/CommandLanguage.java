9239f10bebb1ad5808e81afe51c97cd083a76568
Lightkeks <alexandrelardeux1996@gmail.com>
2021-12-13 10:32:24 +0100

    language files changed and added missing translations (still missing too many)

--- ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandLanguage.java
+++ ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandLanguage.java
@@ -13,8 +13,9 @@ import io.github.davidmc971.modularmsmf.core.util.Utils;
 import io.github.davidmc971.modularmsmf.core.util.ChatUtils.ChatFormat;
 
 /**
- * 
+ *
  * @author David Alexander Pfeiffer (davidmc971)
+ * Permissionless command because it's player specific and not server side (Lightkeks)
  *
  */
 
@@ -28,68 +29,78 @@ public class CommandLanguage implements IModularMSMFCommand {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		LanguageManager languageManager = plugin.getLanguageManager();
 		FileConfiguration language = Utils.configureCommandLanguage(sender, plugin);
 
 		switch (args.length) {
 		case 0:
-			Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.INFO, "coremodule.commands.language.active",
-					"_language",
-					(language.getString("language.id") + " (" + language.getString("language.name") + ") "));
+			Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.INFO, "coremodule.commands.language.active", "_language",
+			(language.getString("language.id") + " (" + language.getString("language.name") + ") "));
 			break;
 		case 1:
-			if (args[0].equalsIgnoreCase("help")) {
-				// TODO[epic=code needed,seq=23] still wip
-			}
-			if (args[0].equalsIgnoreCase("list")) {
-				String out = "[";
-				for (YamlConfiguration languageCfg : languageManager.getAvailableLanguages()) {
-					if (!out.equals("["))
-						out += ", ";
-					out += languageCfg.getString("language.id");
-				}
-				out += "]";
-				Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.LANGUAGE,
-						"coremodule.commands.language.available", "_var", out);
-			} else {
-				Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "coremodule.commands.arguments.missing");
-			}
-			break;
+			return languageSubCmd(sender, command, label, args);
 		case 2:
-			if (args[0].equalsIgnoreCase("set")) {
-				if (sender instanceof Player) {
-					boolean success = false;
-					;
-					for (YamlConfiguration cfg : languageManager.getAvailableLanguages()) {
-						if (cfg.getString("language.id").equals(args[1])) {
-							success = true;
-							break;
-						}
-					}
-					if (success) {
-						plugin.getDataManager().getPlayerCfg(((Player) sender).getUniqueId()).set("language", args[1]);
-						Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS,
-								"coremodule.commands.language.setsuccessplayer");
-					} else {
-						Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR,
-								"coremodule.commands.language.notvalid");
-					}
-				} else {
-					if (plugin.getLanguageManager().setStandardLanguage(args[1])) {
-						Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS,
-								"coremodule.commands.language.setsuccessconsole");
-					} else {
-						Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR,
-								"coremodule.commands.language.notvalid");
-					}
-				}
-			} else if (args[0].equalsIgnoreCase("get")) {
-				// TODO[epic=code needed,seq=24] get other players language
-			}
-			break;
+			return languageSetGet(sender, command, label, args);
 		default:
 			Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "coremodule.commands.arguments.invalid");
 			break;
+		}
+		return true;
+	}
+
+	private boolean languageSetGet(CommandSender sender, Command command, String label, String[] args) {
+		LanguageManager languageManager = plugin.getLanguageManager();
+
+		if (args[0].equalsIgnoreCase("set")) {
+			if (sender instanceof Player) {
+				boolean success = false;
+				;
+				for (YamlConfiguration cfg : languageManager.getAvailableLanguages()) {
+					if (cfg.getString("language.id").equals(args[1])) {
+						success = true;
+						break;
+					}
+				}
+				if (success) {
+					plugin.getDataManager().getPlayerCfg(((Player) sender).getUniqueId()).set("language", args[1]);
+					Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS,
+							"coremodule.commands.language.setsuccessplayer");
+				} else {
+					Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR,
+							"coremodule.commands.language.notvalid");
+				}
+			} else {
+				if (plugin.getLanguageManager().setStandardLanguage(args[1])) {
+					Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.SUCCESS,
+							"coremodule.commands.language.setsuccessconsole");
+				} else {
+					Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR,
+							"coremodule.commands.language.notvalid");
+				}
+			}
+		} else if (args[0].equalsIgnoreCase("get")) {
+			// TODO[epic=code needed,seq=24] get other players language
+		}
+		return true;
+	}
+
+	private boolean languageSubCmd(CommandSender sender, Command command, String label, String[] args) {
+		LanguageManager languageManager = plugin.getLanguageManager();
+
+		if (args[0].equalsIgnoreCase("help")) {
+			// TODO[epic=code needed,seq=23] still wip
+		}
+		if (args[0].equalsIgnoreCase("list")) {
+			String out = "[";
+			for (YamlConfiguration languageCfg : languageManager.getAvailableLanguages()) {
+				if (!out.equals("["))
+					out += ", ";
+				out += languageCfg.getString("language.id");
+			}
+			out += "]";
+			Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.LANGUAGE,
+					"coremodule.commands.language.available", "_var", out);
+		} else {
+			Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "coremodule.commands.arguments.missing");
 		}
 		return true;
 	}
