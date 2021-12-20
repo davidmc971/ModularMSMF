d77976e27e666be1c10e6583e73f9a8a2b58ac68
Lightkeks <alexandrelardeux1996@gmail.com>
2021-12-21 00:14:22 +0100

    complete rewrite, maybe works

--- ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandListPlayers.java
+++ ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandListPlayers.java
@@ -1,135 +1,154 @@
 package io.github.davidmc971.modularmsmf.core.commands;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import io.github.davidmc971.modularmsmf.api.IModularMSMFCommand;
 import io.github.davidmc971.modularmsmf.core.ModularMSMFCore;
 import io.github.davidmc971.modularmsmf.core.PermissionManager;
+import io.github.davidmc971.modularmsmf.core.util.ChatUtils;
 import io.github.davidmc971.modularmsmf.core.util.Utils;
 import io.github.davidmc971.modularmsmf.core.util.ChatUtils.ChatFormat;
+import net.md_5.bungee.api.ChatColor;
 
 /**
- * @author Lightkeks
- * /list <args> || /listall
- * will show online players in mmsmf style (can show online, offline players or both)
+ * @author Lightkeks:
  *
  */
 
 public class CommandListPlayers implements IModularMSMFCommand {
 
-	private ModularMSMFCore plugin;
+    private ModularMSMFCore plugin;
 
-	public CommandListPlayers() {
-		plugin = ModularMSMFCore.Instance();
-	}
+    public CommandListPlayers() {
+        plugin = ModularMSMFCore.Instance();
+    }
 
-    //String[] adjustedList = cfg.get(); //only some idea - maybe good or not - Array?
-    final List<Player> playerListOnline = new ArrayList<>();
-    final List<Player> playerListOffline = new ArrayList<>();
-
-	@Override
-	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
+            @NotNull String[] args) {
         /**
-         * stuff for command
+         * This command is written to show up players if they're online or not.
+         * It can be customized to show up even groups like moderators or admins or
+         * whatever group has been made via NovaPerms.
+         * Only working if sender has permission
          */
-
-        switch(label.toLowerCase()){
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            onlineWithoutSpecialCon.add(player.getName());
+        }
+        if (!PermissionManager.checkPermission(sender, "list_use")) {
+            ChatUtils.sendMsgNoPerm(sender);
+            return true;
+        }
+        switch (label.toLowerCase()) {
             case "list":
+                /**
+                 * This will show up players without vanished or being in spectator.
+                 * Special conditions are given if sender has permission or is op to see even
+                 * vanished or spectator in list.
+                 * Only working if sender has permission
+                 */
+                if (!PermissionManager.checkPermission(sender, "list_players")) {
+                    ChatUtils.sendMsgNoPerm(sender);
+                    return true;
+                }
                 return listPlayers(sender, command, label, args);
             case "listall":
-                return listAllWithoutArgs(sender, command, label, args);
+                /**
+                 * This command will only "work" if sender has permission or is op to see every
+                 * player online on the server regardless of the special conditions given
+                 */
+                if (!PermissionManager.checkPermission(sender, "list_all") | !sender.isOp()) {
+                    ChatUtils.sendMsgNoPerm(sender);
+                    return true;
+                }
+                return listAllPlayers(sender, command, label, args);
         }
         return true;
     }
-    private boolean listAllWithoutArgs(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        /**
-         * likely shortcut for all
-         */
 
-        if(PermissionManager.checkPermission(sender, "list_all_players")){
-            sender.sendMessage("Online: " + playerListOnline);
-            sender.sendMessage("Offline: " + playerListOffline);
-            playerListOnline.clear();
-            playerListOffline.clear();
+    final ArrayList<String> onlineWithoutSpecialCon = new ArrayList<String>();
+    final ArrayList<String> onlineWithSpecialCon = new ArrayList<String>();
+
+    private boolean listAllPlayers(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
+            @NotNull String[] args) {
+        return false;
+    }
+
+    private boolean listPlayers(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
+            @NotNull String[] args) {
+        /**
+         * Here you will see all players without any special conditions if no arguments
+         * are given
+         */
+        if (args.length == 0) {
+            if (sender.isOp() || PermissionManager.checkPermission(sender, "list_all")) {
+                sender.sendMessage("Online: " + onlineWithSpecialCon + onlineWithoutSpecialCon);
+                ;
+            } else {
+                sender.sendMessage("Online: " + onlineWithoutSpecialCon);
+            }
+            return true;
+        }
+        /**
+         * Here it will ask for the arguments to have more functions to each argument
+         */
+        switch (args[0].toLowerCase()) {
+            default:
+                /**
+                 * Will return an invalid argument if nothing matches any cases down below
+                 */
+                Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR,
+                        "coremodule.commands.arguments.invalid");
+                break;
+            case "help":
+            case "group":
+                /**
+                 * here you can show up like other arguments or groups to list in chat
+                 */
+                return useAsPlayer(sender, command, label, args);
+            case "settings":
+                /**
+                 * only useful in combination with other modules which work together
+                 */
+                if (!PermissionManager.checkPermission(sender, "list_admin_settings") || !sender.isOp()) {
+                    ChatUtils.sendMsgNoPerm(sender);
+                    return true;
+                } else {
+                    return useAsAdmin(sender, command, label, args);
+                }
+        }
+        return true;
+    }
+
+    private boolean useAsAdmin(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
+            @NotNull String[] args) {
+        // TODO: have to write stuff down here
+        return false;
+    }
+
+    private boolean useAsPlayer(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
+            @NotNull String[] args) {
+        if (!PermissionManager.checkPermission(sender, "list_use_groups")) {
+            ChatUtils.sendMsgNoPerm(sender);
             return true;
         } else {
-            Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.NOPERM, "coremodule.player.nopermission");
-        }
-        return true;
-    }
-    private boolean listPlayers(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+            if (args[1].equalsIgnoreCase("help")) {
+                sender.sendMessage("/list");
+                sender.sendMessage("/list help");
+                sender.sendMessage("/list group");
+                sender.sendMessage("/list settings" + ChatColor.GRAY + " [admin/op only]");
+            }
+            if (args[1].equalsIgnoreCase("group")) {
 
-        for (final Player player : Bukkit.getOnlinePlayers()) {
-            playerListOnline.add(player);
+            }
         }
-        for (OfflinePlayer player : Bukkit.getOfflinePlayers()) { //FIXME: needs to put all offline players in a list (likely for moderator and higher ranks)
-            //playerListOffline.add(player); //FIXME
-        }
-
-        switch(args.length){
-            /**
-             * TODO: list can be adjusted later with configs to even show groups like admin/moderator/etc. (online and offline)
-             */
-            case 0:
-                if(sender instanceof ConsoleCommandSender){ //shows all players even without args
-                    sender.sendMessage("Online: " + playerListOnline);
-                    sender.sendMessage("Offline: " + playerListOffline);
-                    playerListOnline.clear();
-                    playerListOffline.clear();
-                    return true;
-                }
-                if((sender.isOp()) || ((PermissionManager.checkPermission(sender, "list_all_players")))){ //shows all players if sender is op or has permission for it
-                    sender.sendMessage("Online: " + playerListOnline);
-                    sender.sendMessage("Offline: " + playerListOffline);
-                    playerListOnline.clear();
-                    playerListOffline.clear();
-                    return true;
-                }
-                if(!sender.isOp() && PermissionManager.checkPermission(sender, "list_players_online")){ //shows players which are online
-                    sender.sendMessage("Online: " + playerListOnline);
-                    playerListOnline.clear();
-                    return true;
-                } else {
-                    Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.NOPERM, "coremodule.player.nopermission");
-                }
-                break;
-            case 1:
-                if(args[0].equalsIgnoreCase("help")){
-                    /**
-                     * list all related arguments to show as help
-                     */
-                    return true;
-                }
-                if(args[0].equalsIgnoreCase("offline") || args[0].equalsIgnoreCase("off")){
-                    if(PermissionManager.checkPermission(sender, "list_players_offline")){
-                        sender.sendMessage("Offline: " + playerListOffline);
-                        playerListOffline.clear();
-                        return true;
-                    } else {
-                        Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.NOPERM, "coremodule.player.nopermission");
-                    }
-                    return true;
-                } else {
-                    /**
-                     * here will be the adjustable code for lists like moderator/admin/etc. (online and offline)
-                     */
-                    sender.sendMessage("nope"); //only for testing purposes
-                }
-                break;
-            default:
-                Utils.sendMessageWithConfiguredLanguage(plugin, sender, ChatFormat.ERROR, "coremodule.commands.arguments.toomany");
-                break;
-        }
-        return true;
+        return false;
     }
 
     @Override
@@ -139,7 +158,7 @@ public class CommandListPlayers implements IModularMSMFCommand {
 
     @Override
     public String[] Aliases() {
-        return new String[] { "listall" };
+        return null;
     }
 
     @Override
