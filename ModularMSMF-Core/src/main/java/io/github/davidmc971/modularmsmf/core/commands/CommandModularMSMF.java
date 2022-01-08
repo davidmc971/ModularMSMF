7b866a60cdf348ae8cea9570ac81bcfd7e5fcbbb
Lightkeks <alexandrelardeux1996@gmail.com>
2022-01-08 15:16:57 +0100

    manual fixes - not done yet

--- ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandModularMSMF.java
+++ ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandModularMSMF.java
@@ -2,6 +2,8 @@ package io.github.davidmc971.modularmsmf.core.commands;
 
 import java.io.File;
 
+import com.iridium.iridiumcolorapi.IridiumColorAPI;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -38,66 +40,95 @@ public class CommandModularMSMF implements IModularMSMFCommand {
 		plugin = ModularMSMFCore.Instance();
 	}
 
-	private String getBukkitVersion = Bukkit.getBukkitVersion();
-	private String getName = Bukkit.getName();
-	private String getVersion = Bukkit.getVersion();
-	private String getMinecraftVersion = Bukkit.getMinecraftVersion();
-	private int getMaxPlayers = Bukkit.getMaxPlayers();
+	private String bukkitVersion = Bukkit.getBukkitVersion();
+	private String name = Bukkit.getName();
+	private String version = Bukkit.getVersion();
+	private String mcversion = Bukkit.getMinecraftVersion();
+	private int maxplayers = Bukkit.getMaxPlayers();
 
 	private final String infoPrefix = ChatUtils.getFormattedPrefix(ChatUtils.ChatFormat.INFO);
-	//private String errorPrefix = ChatUtils.getFormattedPrefix(ChatUtils.ChatFormat.ERROR);
+	// private String errorPrefix =
+	// ChatUtils.getFormattedPrefix(ChatUtils.ChatFormat.ERROR);
 	private final String debugPrefix = ChatUtils.getFormattedPrefix(ChatUtils.ChatFormat.DEBUG);
 
 	@Override
-	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-		if(sender instanceof ConsoleCommandSender){
-			Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.CONSOLE, "coremodule.noconsole");
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
+			@NotNull String[] args) {
+		if (!PermissionManager.checkPermission(sender, "mmsmf") || sender instanceof ConsoleCommandSender) {
+			ChatUtils.sendMsgNoPerm(sender);
 			return true;
 		}
-		if (PermissionManager.checkPermission(sender, "mmsmf")) {
-			switch(args.length){
-				case 0:
-					return mmsmfCmd(sender, command, label, args);
-				case 1:
-					return mmsmfCmdMore(sender, command, label, args);
-				default:
-					Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.commands.arguments.toomany");
+		switch (args.length) {
+			case 0:
+				return mmsmfCmd(sender, command, label, args);
+			case 1:
+				return mmsmfCmdMore(sender, command, label, args);
+			default:
+				Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR,
+						"coremodule.commands.arguments.toomany");
 				break;
-			}
-		} else {
-			Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.NOPERM, "coremodule.player.nopermission");
 		}
 		return true;
 	}
 
 	private boolean mmsmfCmd(CommandSender sender, Command command, String label, String[] args) {
 
-		FileConfiguration language = Utils.configureCommandLanguage(sender); //get the language string for selected language files
+		FileConfiguration language = Utils.configureCommandLanguage(sender); // get the language string for selected
+																				// language files
 
-		// FIXME: Just put this back here temporarily for easy insight into relevant development information.
-		sender.sendMessage(infoPrefix + "Server Type: " + getName);
+		// FIXME: Just put this back here temporarily for easy insight into relevant
+		// development information.
+		sender.sendMessage(infoPrefix + "Server Type: " + name);
 		sender.sendMessage(infoPrefix + "Plugin Version: " + ChatColor.GREEN + plugin.pluginver);
 		sender.sendMessage(
-				infoPrefix + "Server Version: " + ChatColor.YELLOW + getBukkitVersion);
+				infoPrefix + "Server Version: " + ChatColor.YELLOW + bukkitVersion);
 		sender.sendMessage(infoPrefix + "Developers: " + ChatColor.LIGHT_PURPLE + plugin.authors);
 		sender.sendMessage(
 				debugPrefix + "Build Timestamp: " + ChatColor.YELLOW + plugin.getDebugTimestamp());
-		sender.sendMessage("Test: " + getName + " <-- getName , " + getBukkitVersion + " <-- getBukkitVersion , " + getVersion + " <-- getVersion , " + getMinecraftVersion + " <-- getMinecraftVersion , " + getMaxPlayers + " <-- getMaxPlayers(int) , ");
+		sender.sendMessage("Test: " + name + " <-- getName , " + bukkitVersion + " <-- getBukkitVersion , "
+				+ version + " <-- getVersion , " + mcversion + " <-- getMinecraftVersion , "
+				+ maxplayers + " <-- getMaxPlayers(int) , ");
 
-		Component testComponent = Component.text(infoPrefix + "More help: ").style(Style.empty().decorate(TextDecoration.BOLD))
-		.append(Component.text("info").color(TextColor.color(127, 127, 127)).hoverEvent(net.kyori.adventure.text.event.HoverEvent.showText(Component.text(language.getString("coremodule.commands.mmsmf.info")))).clickEvent(net.kyori.adventure.text.event.ClickEvent.runCommand("/mmsmf info"))) //clickable event for "info"
-		.append(Component.text(" ")) //space in text
-		.append(Component.text("report a bug").color(TextColor.color(255, 127, 127)).hoverEvent(net.kyori.adventure.text.event.HoverEvent.showText(Component.text(language.getString("coremodule.commands.mmsmf.report")))).clickEvent(net.kyori.adventure.text.event.ClickEvent.runCommand("/mmsmf report"))) //clickable event for "report a bug"
-		.append(Component.text(" ")) //space in text
-		.append(Component.text("teamspeak").color(TextColor.color(127, 127, 127)).hoverEvent(net.kyori.adventure.text.event.HoverEvent.showText(Component.text(language.getString("coremodule.commands.mmsmf.teamspeak.click")))).clickEvent(net.kyori.adventure.text.event.ClickEvent.runCommand("/mmsmf teamspeak"))) //clickable event for "teamspeak"
-		.append(Component.text(" ")) //space in text
-		.append(Component.text("discord").color(TextColor.color(127, 127, 255)).hoverEvent(net.kyori.adventure.text.event.HoverEvent.showText(Component.text(language.getString("coremodule.commands.mmsmf.discord.click")))).clickEvent(net.kyori.adventure.text.event.ClickEvent.runCommand("/mmsmf discord"))); //clickable event for "discord"
+		Component testComponent = Component.text(infoPrefix + "More help: ")
+				.style(Style.empty().decorate(TextDecoration.BOLD))
+				.append(Component.text("info").color(TextColor.color(127, 127, 127))
+						.hoverEvent(net.kyori.adventure.text.event.HoverEvent
+								.showText(Component.text(language.getString("coremodule.commands.mmsmf.info"))))
+						.clickEvent(net.kyori.adventure.text.event.ClickEvent.runCommand("/mmsmf info")))
+				.append(Component.text(" "))
+				.append(Component.text("report a bug").color(TextColor.color(255, 127, 127))
+						.hoverEvent(net.kyori.adventure.text.event.HoverEvent
+								.showText(Component.text(language.getString("coremodule.commands.mmsmf.report"))))
+						.clickEvent(net.kyori.adventure.text.event.ClickEvent.runCommand("/mmsmf report")))
+				.append(Component.text(" "))
+				.append(Component.text("teamspeak").color(TextColor.color(127, 127, 127))
+						.hoverEvent(net.kyori.adventure.text.event.HoverEvent.showText(
+								Component.text(language.getString("coremodule.commands.mmsmf.teamspeak.click"))))
+						.clickEvent(net.kyori.adventure.text.event.ClickEvent.runCommand("/mmsmf teamspeak")))
+				.append(Component.text(" "))
+				.append(Component.text("discord").color(TextColor.color(127, 127, 255))
+						.hoverEvent(net.kyori.adventure.text.event.HoverEvent.showText(
+								Component.text(language.getString("coremodule.commands.mmsmf.discord.click"))))
+						.clickEvent(net.kyori.adventure.text.event.ClickEvent.runCommand("/mmsmf discord")));
 		sender.sendMessage(Identity.nil(), testComponent, MessageType.SYSTEM);
+		// TODO: testing iridiumcolorapi
+		// solid tag
+		sender.sendMessage(IridiumColorAPI.process("<SOLID:8200FF>REEEEEEEEEEEEEE"));
+		sender.sendMessage("Solid tag: <SOLID:8200FF>'string'");
+		// gradient tag
+		sender.sendMessage(IridiumColorAPI.process("<GRADIENT:2C08BA>Cool string with a gradient</GRADIENT:028A97>"));
+		sender.sendMessage("Gradient tag: <GRADIENT:2C08BA>'string'</GRADIENT:028A97>");
+		// rainbow1
+		sender.sendMessage(IridiumColorAPI.process("<RAINBOW1>THIS IS A REALLY COOL Rainbow</RAINBOW>"));
+		sender.sendMessage("Rainbow1 tag: <RAINBOW1>'string'</RAINBOW>");
+		// rainbow100
+		sender.sendMessage(IridiumColorAPI.process("<RAINBOW100>THIS IS A REALLY COOL Rainbow</RAINBOW>"));
+		sender.sendMessage("Rainbow100 tag: <RAINBOW100>'string'</RAINBOW>");
 		return true;
 	}
 
 	private boolean mmsmfCmdMore(CommandSender sender, Command command, String label, String[] args) {
-		switch(args[0].toLowerCase()){
+		switch (args[0].toLowerCase()) {
 			case "info":
 				return mmsmfInfo(sender, command, label, args);
 			case "report":
@@ -107,31 +138,32 @@ public class CommandModularMSMF implements IModularMSMFCommand {
 			case "teamspeak":
 				return mmsmfTeamspeak(sender, command, label, args);
 			default:
-				Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.commands.arguments.invalid");
+				Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR,
+						"coremodule.commands.arguments.invalid");
 				break;
 		}
 		return true;
 	}
 
 	private boolean mmsmfInfo(CommandSender sender, Command command, String label, String[] args) {
-		if(args.length == 1){
+		if (args.length == 1) {
 			sender.sendMessage(infoPrefix + "Plugin Version: " + ChatColor.GREEN + plugin.pluginver);
 			sender.sendMessage(infoPrefix + "Server's running at: " + ChatColor.YELLOW + Bukkit.getVersion());
 			sender.sendMessage(infoPrefix + "Developer: " + ChatColor.LIGHT_PURPLE + plugin.authors);
 			sender.sendMessage(debugPrefix + "Build Timestamp: " + ChatColor.YELLOW + plugin.getDebugTimestamp());
-			} else {
-				Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.commands.arguments.toomany");
-			}
-		return true;
+			return true;
 		}
+		Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.commands.arguments.toomany");
+		return true;
+	}
 
 	private boolean mmsmfReport(CommandSender sender, Command command, String label, String[] args) {
 		/**
-		* TODO: will be implemented. otherwise discord/github for reporting bugs.
-		*/
+		 * TODO: will be implemented. otherwise discord/github for reporting bugs.
+		 */
 		Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.notimplementedyet");
 		return true;
-		}
+	}
 
 	private boolean mmsmfDiscord(CommandSender sender, Command command, String label, String[] args) {
 		if (args.length == 1) {
@@ -140,33 +172,37 @@ public class CommandModularMSMF implements IModularMSMFCommand {
 				YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
 				if (cfg.contains("discordID") && !cfg.getString("discordID").equals("")) {
 					String discordID = cfg.getString("discordID");
-					Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.INFO, "coremodule.commands.mmsmf.discord.link", "_link", discordID);
+					Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.INFO,
+							"coremodule.commands.mmsmf.discord.link", "_link", discordID);
 					return true;
 				} else {
-					Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.commands.mmsmf.discord.linkmissing");
+					Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR,
+							"coremodule.commands.mmsmf.discord.linkmissing");
 				}
 			} else {
-				sender.sendMessage(ChatColor.DARK_RED + "file should be created!"); //TODO: only for testing purposes
+				sender.sendMessage(ChatColor.DARK_RED + "file should be created!"); // TODO: only for testing purposes
 			}
 		} else {
 			Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.commands.arguments.toomany");
 		}
-			return true;
-		}
+		return true;
+	}
 
 	private boolean mmsmfTeamspeak(CommandSender sender, Command command, String label, String[] args) {
-		if(args.length == 1){
+		if (args.length == 1) {
 			File file = new File("plugins/ModularMSMF/settings.yml");
 			if (file.exists()) {
 				YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
 				if (cfg.contains("teamspeakIP") && !cfg.getString("teamspeakIP").equals("")) {
 					String teamspeakIP = cfg.getString("teamspeakIP");
-					Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.INFO, "coremodule.commands.mmsmf.teamspeak.ip", "_ip", teamspeakIP);
+					Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.INFO,
+							"coremodule.commands.mmsmf.teamspeak.ip", "_ip", teamspeakIP);
 				} else {
-					Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.commands.mmsmf.teamspeak.ipmissing");
+					Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR,
+							"coremodule.commands.mmsmf.teamspeak.ipmissing");
 				}
 			} else {
-				sender.sendMessage(ChatColor.DARK_RED + "file should be created!"); //TODO: only for testing purposes
+				sender.sendMessage(ChatColor.DARK_RED + "file should be created!"); // TODO: only for testing purposes
 			}
 		} else {
 			Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.commands.arguments.toomany");
--- ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandServerInfo.java
+++ ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandServerInfo.java
@@ -1,11 +1,14 @@
 package io.github.davidmc971.modularmsmf.core.commands;
 
+import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 import io.github.davidmc971.modularmsmf.api.IModularMSMFCommand;
 import io.github.davidmc971.modularmsmf.core.PermissionManager;
+import io.github.davidmc971.modularmsmf.core.util.ChatUtils;
 import io.github.davidmc971.modularmsmf.core.util.Utils;
 import io.github.davidmc971.modularmsmf.core.util.ChatUtils.ChatFormat;
 
@@ -18,13 +21,29 @@ import io.github.davidmc971.modularmsmf.core.util.ChatUtils.ChatFormat;
 
 public class CommandServerInfo implements IModularMSMFCommand {
 
+	private String[] convert = Bukkit.getBukkitVersion().split("[-\\:]");
+	private ArrayList<String> convertedList = new ArrayList<String>();
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-		if (PermissionManager.checkPermission(sender, "serverinfo")) {
-			Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.INFO, "coremodule.bukkitversion",
-					"_bukkitver", Bukkit.getBukkitVersion());
+		if (!PermissionManager.checkPermission(sender, "serverinfo")) {
+			ChatUtils.sendMsgNoPerm(sender);
 		}
+		if (args.length < 0) {
+			Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.argument.toomany");
+			return true;
+		}
+		for(int i = 0; i < 1; i++){
+			convertedList.add(convert[i].toString());
+		}
+		//FIXME: WIP, [] <-- needed to remove
+		convertedList.trimToSize();
+		String removeable = "[]";
+		String replaceable = "";
+		System.out.println(convertedList.toString());
+		System.out.println(convertedList.toString().replace(removeable, replaceable));
+		Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.INFO, "coremodule.bukkitversion",
+				"_bukkitver", convertedList.toString().replace(removeable, ""));
 		return true;
 	}
 
