f5da2adea81dc1fb0c9c1c124bb5d13449d4ad73
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2022-01-08 17:31:22 +0100

    fixed display of mc version in serverinfo

--- ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandServerInfo.java
+++ ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandServerInfo.java
@@ -21,29 +21,30 @@ import io.github.davidmc971.modularmsmf.core.util.ChatUtils.ChatFormat;
 
 public class CommandServerInfo implements IModularMSMFCommand {
 
-	private String[] convert = Bukkit.getBukkitVersion().split("[-\\:]");
-	private ArrayList<String> convertedList = new ArrayList<String>();
+	// private String[] convert = Bukkit.getBukkitVersion().split("[-\\:]");
+	// private ArrayList<String> convertedList = new ArrayList<String>();
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (!PermissionManager.checkPermission(sender, "serverinfo")) {
 			ChatUtils.sendMsgNoPerm(sender);
 		}
-		if (args.length < 0) {
+		if (args.length > 0) {
 			Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.argument.toomany");
 			return true;
 		}
-		for(int i = 0; i < 1; i++){
-			convertedList.add(convert[i].toString());
-		}
-		//FIXME: WIP, [] <-- needed to remove
-		convertedList.trimToSize();
-		String removeable = "[]";
-		String replaceable = "";
-		System.out.println(convertedList.toString());
-		System.out.println(convertedList.toString().replace(removeable, replaceable));
+		// for(int i = 0; i < 1; i++){
+		// 	convertedList.add(convert[i].toString());
+		// }
+		// //FIXME: WIP, [] <-- needed to remove
+		// convertedList.trimToSize();
+		// String removeable = "[]";
+		// String replaceable = "";
+		// System.out.println(convertedList.toString());
+		// System.out.println(convertedList.toString().replace(removeable, replaceable));
+
 		Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.INFO, "coremodule.bukkitversion",
-				"_bukkitver", convertedList.toString().replace(removeable, ""));
+				"_bukkitver", Bukkit.getBukkitVersion().split("[-\\:]")[0]);
 		return true;
 	}
 
