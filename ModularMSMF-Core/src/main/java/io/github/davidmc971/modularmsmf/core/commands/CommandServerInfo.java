51f65dcbd310c513bd0f4d362d61cb6046fcb698
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2022-01-09 01:28:13 +0100

    cleanup

--- ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandServerInfo.java
+++ ModularMSMF-Core/src/main/java/io/github/davidmc971/modularmsmf/core/commands/CommandServerInfo.java
@@ -1,7 +1,5 @@
 package io.github.davidmc971.modularmsmf.core.commands;
 
-import java.util.ArrayList;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -20,9 +18,6 @@ import io.github.davidmc971.modularmsmf.core.util.ChatUtils.ChatFormat;
  */
 
 public class CommandServerInfo implements IModularMSMFCommand {
-
-	// private String[] convert = Bukkit.getBukkitVersion().split("[-\\:]");
-	// private ArrayList<String> convertedList = new ArrayList<String>();
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -33,16 +28,6 @@ public class CommandServerInfo implements IModularMSMFCommand {
 			Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "coremodule.argument.toomany");
 			return true;
 		}
-		// for(int i = 0; i < 1; i++){
-		// 	convertedList.add(convert[i].toString());
-		// }
-		// //FIXME: WIP, [] <-- needed to remove
-		// convertedList.trimToSize();
-		// String removeable = "[]";
-		// String replaceable = "";
-		// System.out.println(convertedList.toString());
-		// System.out.println(convertedList.toString().replace(removeable, replaceable));
-
 		Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.INFO, "coremodule.bukkitversion",
 				"_bukkitver", Bukkit.getBukkitVersion().split("[-\\:]")[0]);
 		return true;
