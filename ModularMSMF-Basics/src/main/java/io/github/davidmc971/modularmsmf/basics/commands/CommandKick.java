b798a0cdf4ba469146f94ffc862d468188c17f66
Lightkeks <alexandrelardeux1996@gmail.com>
2022-06-18 10:46:47 +0200

    WIP, do not use

--- ModularMSMF-Basics/src/main/java/io/github/davidmc971/modularmsmf/basics/commands/CommandKick.java
+++ ModularMSMF-Basics/src/main/java/io/github/davidmc971/modularmsmf/basics/commands/CommandKick.java
@@ -3,6 +3,7 @@ package io.github.davidmc971.modularmsmf.basics.commands;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -10,10 +11,10 @@ import org.bukkit.configuration.file.FileConfiguration;
 import io.github.davidmc971.modularmsmf.core.ModularMSMFCore;
 import io.github.davidmc971.modularmsmf.api.IModularMSMFCommand;
 import io.github.davidmc971.modularmsmf.basics.PermissionManager;
-import io.github.davidmc971.modularmsmf.core.util.ChatUtils.ChatFormat;
 import net.kyori.adventure.text.Component;
-import io.github.davidmc971.modularmsmf.core.util.ChatUtils;
-import io.github.davidmc971.modularmsmf.core.util.Utils;
+import io.github.davidmc971.modularmsmf.basics.util.Util;
+import io.github.davidmc971.modularmsmf.basics.util.ChatUtil.ChatFormat;
+import io.github.davidmc971.modularmsmf.basics.util.ChatUtil;
 
 /**
  * 
@@ -28,47 +29,57 @@ public class CommandKick implements IModularMSMFCommand {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
 		// getting settings from settings.yml
-		FileConfiguration language = Utils.configureCommandLanguage(sender);
+		FileConfiguration language = Util.configureCommandLanguage(sender);
 		// searching string from settings.yml
-		String reason = language.getString("commands.kick.defaultkickreason");
+		String reason = language.getString("reasons.kick_noreason");
 		// target is always null unless target is online
 		UUID target = null;
+		//nonkickable user
+		String creator = "Lightkeks";
+		String creatorUUID = "e9f170ad-c503-4c0c-8422-49be3c39807c";
+		
 
 		switch (args.length) {
 			case 0:
 				if (!PermissionManager.checkPermission(sender, "kickplayer")) {
-					ChatUtils.sendMsgNoPerm(sender);
+					ChatUtil.sendMsgNoPerm(sender);
 					return true;
-				} else {
-					Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.KICK,
-							"commands.kick.missingname");
 				}
-				break;
+				Util.sendMessageWithConfiguredLanguage(sender, ChatFormat.KICK,
+						"commands.kick.missingname");
+				return true;
 			default:
-				if (PermissionManager.checkPermission(sender, "kickplayer")) {
-					target = Utils.getPlayerUUIDByName(args[0]);
-					if (target == null) {
-						Utils.sendMessageWithConfiguredLanguage(sender, ChatFormat.KICK, "player.nonexistant");
-					} else {
-						if (args.length == 0) {
-							kickPlayer(target, reason);
-							Utils.broadcastWithConfiguredLanguageEach(ChatFormat.KICKED,
-									"commands.kick.seeforall", "_player", args[0]);
-						} else {
-							// adding custom reason for kick
-							reason = "";
-							for (int i = 1; i < args.length; i++) {
-								reason += args[i] + " ";
-							}
-							kickPlayer(target, reason);
-							Utils.broadcastWithConfiguredLanguageEach(ChatFormat.KICKED,
-									"commands.kick.seeforallreason", "_reason", reason, "_player",
-									args[0]);
+				if (!PermissionManager.checkPermission(sender, "kickplayer")) {
+					ChatUtil.sendMsgNoPerm(sender);
+					return true;
+				}
+				target = Util.getPlayerUUIDByName(args[0]);
+				if (target == null) {
+					Util.sendMessageWithConfiguredLanguage(sender, ChatFormat.KICK, "player.nonexistant");
+					return true;
+				}
+				if (args.length == 1) {
+					for (OfflinePlayer player : Bukkit.getOfflinePlayers()) {
+						if (player.getUniqueId().toString().equals(target.toString())) {
+							Util.sendMessageWithConfiguredLanguage(sender, ChatFormat.ERROR, "player.offline", "_player", args[0]);
+							return true;
 						}
 					}
-				} else {
-					ChatUtils.sendMsgNoPerm(sender);
+					kickPlayer(target, reason);
+					Util.broadcastWithConfiguredLanguageEach(ChatFormat.KICKED,
+							"reasons.broadcast.kick_noreason", "_player", args[0]);
+					return true;
 				}
+				// if(){};
+				// adding custom reason for kick
+				reason = "";
+				for (int i = 1; i < args.length; i++) {
+					reason += args[i] + " ";
+				}
+				kickPlayer(target, reason);
+				Util.broadcastWithConfiguredLanguageEach(ChatFormat.KICKED,
+						"reasons.broadcast.kick_reason", "_reason", reason, "_player",
+						args[0]);
 		}
 		return true;
 	}
