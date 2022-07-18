83b6d025690e87de6404e7f5b0ae32dbf4e616d0
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2022-07-19 00:33:18 +0200

    quality of development improved ;) testserver script and buildDev task with sh to remember it

--- testserver/start.sh
+++ testserver/start.sh
@@ -1,4 +1,7 @@
-until java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx4G -Xms4G -jar paper-1.19-20.jar; do
-    echo "Server 'myserver' crashed with exit code $?.  Respawning.." >&2
+#! /usr/bin/sh
+
+# TODO: inject `rl confirm` when jars change
+until java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx4G -Xms4G -jar $1 nogui; do
+    echo "Server 'testserver' crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
