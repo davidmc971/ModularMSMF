55b4d27cfd6a40a736ef6456c6d6a44dd18b70d4
Alexandre Lardeux <alexandrelardeux1996@gmail.com>
2022-12-15 23:50:39 +0100

    changed way of start bat file

--- testserver/start.bat
+++ testserver/start.bat
@@ -1,5 +1,6 @@
 @echo off
+set /p input= Type in the name of the jar to continue...
 :1
-java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx2G -Xms512M -jar paper-1.19-20.jar nogui
+java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xmx2G -Xms512M -jar %input%.jar nogui
 goto 1
 pause
