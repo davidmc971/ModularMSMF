6b98e42fd647a4144d0c0412d25e08b8d33f5da6
Alexandre Lardeux <alexandrelardeux1996@gmail.com>
2023-01-19 22:44:31 +0100

    removed unneccessary code

--- ModularMSMF-CommandBlocker/src/main/java/io/github/davidmc971/modularmsmf/commandblocker/PermissionManager.java
+++ ModularMSMF-CommandBlocker/src/main/java/io/github/davidmc971/modularmsmf/commandblocker/PermissionManager.java
@@ -13,20 +13,17 @@ import org.bukkit.command.CommandSender;
 public class PermissionManager {
 
 	private static final HashMap<String, String> permissions = new HashMap<String, String>() {
-		/**
-		*
-		*/
 		private static final long serialVersionUID = 263473457634L;
 		{
-        //commandblocker permissions
-        put("use","modularmsmf.commandblocker.use");
-	}};
+			put("use", "modularmsmf.commandblocker.use");
+		}
+	};
 
 	public static boolean checkPermission(CommandSender sender, String permission) {
 		return sender.hasPermission(getPermission(permission));
 	}
 
 	public static String getPermission(String name) {
-			return permissions.get(name);
+		return permissions.get(name);
 	}
 }
