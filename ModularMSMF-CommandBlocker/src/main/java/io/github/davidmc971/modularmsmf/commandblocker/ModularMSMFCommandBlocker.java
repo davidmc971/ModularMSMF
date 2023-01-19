06f4fef3917daa6b0abaa08ba71a892cb2b9a195
Alexandre Lardeux <alexandrelardeux1996@gmail.com>
2023-01-19 22:57:07 +0100

    Update ModularMSMFCommandBlocker.java

--- ModularMSMF-CommandBlocker/src/main/java/io/github/davidmc971/modularmsmf/commandblocker/ModularMSMFCommandBlocker.java
+++ ModularMSMF-CommandBlocker/src/main/java/io/github/davidmc971/modularmsmf/commandblocker/ModularMSMFCommandBlocker.java
@@ -4,7 +4,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import io.github.davidmc971.modularmsmf.commandblocker.configuration.BlockedCommands;
 
-//@SuppressWarnings("unused")
 public class ModularMSMFCommandBlocker extends JavaPlugin {
     private static ModularMSMFCommandBlocker instance = null;
 
